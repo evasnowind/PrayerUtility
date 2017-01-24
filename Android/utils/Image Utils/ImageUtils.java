29cc4df333e349d4b23e7d35c12179021df5a0b1
yuchenglong <yuchenglong8809@163.com>
2017-01-24 10:52:13 +0800

    update ImageUtils: add image save method

--- Android/utils/Image Utils/ImageUtils.java
+++ Android/utils/Image Utils/ImageUtils.java
@@ -1,12 +1,20 @@
 package com.prayer.testdemo;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 public class ImageUtils {
@@ -57,5 +65,72 @@ public class ImageUtils {
 			//TODO throw exception or do something
 		}
 		return uri;
+	}
+	
+	private static void saveImageByUri(Activity activity, Uri uri, String destPath, String name){
+		InputStream is = null;
+		
+		try {
+			is = activity.getContentResolver().openInputStream(uri);
+			
+			saveImageToSD(is, destPath, name);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally{
+			if(is != null){
+				try {
+					is.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
+
+	private static void saveImageToSD(InputStream is, String destPath, String imageName){
+		String sdPath = Environment.getExternalStorageDirectory().getPath();
+		
+		//TODO ++++++++++++++++++++++++++++++++++++++++++
+		//添加SD相关判断
+		Bitmap bitmap = BitmapFactory.decodeStream(is);
+		
+		File file = new File(sdPath+"/"+destPath);
+		File picFile = null;
+	
+		FileOutputStream fos = null;
+		try {
+			
+			picFile = new File(file, imageName);
+			if(!picFile.exists()){
+				picFile.createNewFile();
+			}
+			
+			fos = new FileOutputStream(picFile);
+			
+			if(!TextUtils.isEmpty(imageName) && imageName.endsWith(".png")){
+				bitmap.compress(CompressFormat.PNG, 100, fos);
+			} else if(!TextUtils.isEmpty(imageName) 
+					&& (imageName.endsWith(".jpg") || imageName.endsWith(".jpeg"))){
+				bitmap.compress(CompressFormat.JPEG, 100, fos);
+			} else {
+				//log, throw exception or do something
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			if(fos != null){
+				try {
+					fos.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
