b864c01678ca3c8443ce5cae2e7389d58d4c8862
yuchenglong <yuchenglong@abchina.com>
2018-02-19 16:26:07 +0800

    add dubbo demo

---
+++ Java/frameworks/dubbo/DubboDemo/dubbo-consumer2/src/main/java/com/prayerlaputa/demo/consumer/Consumer.java
@@ -0,0 +1,18 @@
+package com.prayerlaputa.demo.consumer;
+
+import com.prayerlaputa.demo.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+    public static void main(String[] args) {
+        //测试常规服务
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("demo-consumer.xml");
+        context.start();
+        System.out.println("consumer2 start");
+        DemoService demoService = context.getBean(DemoService.class);
+        System.out.println("consumer2");
+        System.out.println(demoService.getPermissions(4L));
+    }
+
+}
